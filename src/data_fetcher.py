c6f2274bc138875e3faaf30d067b755bab270321
Niki Nakamura <niki.nakamu@gmail.com>
2025-02-25 19:09:51 +0900

    Update data_fetcher.py

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -1,56 +1,43 @@
 # data_fetcher.py
-import json
-import pandas as pd
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import streamlit as st  # ← Secretsを読むため
-
-# ここは、該当スプレッドシートの ID を設定
-SPREADSHEET_ID = "1X6beVQYQsCKl2EV7yHg4y182EI0RJdJBaNGtcuX2bwo"
-SHEET_NAME = "query_貼付"
-
-def get_gspread_client():
-    """
-    Streamlit の Secrets に格納した Service Account JSON を用いて認証し、
-    gspread.Client を返す。
-    """
-    # secrets.toml で定義: [キー] GSPREAD_SERVICE_ACCOUNT_JSON = """{ ... }"""
-    service_account_json_str = st.secrets["GSPREAD_SERVICE_ACCOUNT_JSON"]
-    
-    # JSON文字列をPythonのdictに変換
-    info = json.loads(service_account_json_str)
-
-    scopes = [
-        "https://www.googleapis.com/auth/spreadsheets.readonly",
-        "https://www.googleapis.com/auth/drive.readonly",
-    ]
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(info, scopes)
-    gc = gspread.authorize(credentials)
-    return gc
-
-def fetch_data_from_query_sheet() -> pd.DataFrame:
-    """
-    "query_貼付" シートのデータを DataFrame として返す。
-    1行目をカラム名、2行目以降をレコードとみなす想定。
-    """
-    gc = get_gspread_client()
-    sh = gc.open_by_key(SPREADSHEET_ID)
-    wks = sh.worksheet(SHEET_NAME)
-
-    all_values = wks.get_all_values()
-    if len(all_values) < 2:
-        # ヘッダーしかない or シートが空
-        return pd.DataFrame()
-
-    header = all_values[0]
-    data_rows = all_values[1:]
-    df = pd.DataFrame(data_rows, columns=header)
-    return df
+import pandas as pd
 
 def main_fetch_all():
     """
-    シート 'query_貼付' を取得し、CSVに保存するのみ。
+    Google Sheets 'query_貼付' シートから最新データを取得し、
+    'sheet_query_data.csv' というCSVに書き出す。
     """
-    df = fetch_data_from_query_sheet()
+
+    # 1) 認証設定
+    scope = [
+        'https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/drive'
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        'credentials.json',  # サービスアカウントJSONファイル
+        scope
+    )
+    client = gspread.authorize(creds)
+
+    # 2) スプレッドシートを開く
+    #    SPREADSHEET_KEY はURLの「/d/」と「/edit」間にある部分
+    SPREADSHEET_KEY = '1jnxiqozvo5EQa30Yl-Tk9AH-raMsnpCPGsQ_C2io4Ik'
+    SHEET_NAME = 'query_貼付'
+
+    sh = client.open_by_key(SPREADSHEET_KEY)
+    worksheet = sh.worksheet(SHEET_NAME)
+
+    # 3) シート全体の値を取得して pandas.DataFrame に変換
+    data = worksheet.get_all_values()
+    if not data:
+        df = pd.DataFrame()
+    else:
+        headers = data[0]
+        rows = data[1:]
+        df = pd.DataFrame(rows, columns=headers)
+
+    # 4) CSVに書き出し
     df.to_csv("sheet_query_data.csv", index=False, encoding="utf-8-sig")
-    print("シート 'query_貼付' のデータを 'sheet_query_data.csv' に保存しました。")
+    print("Data has been fetched from Sheets and saved to sheet_query_data.csv!")
